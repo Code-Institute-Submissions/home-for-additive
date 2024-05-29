158302a04b962be55fc77a9be2f6453cee91cf97
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-05-29 14:24:55 +0000

    Migrate theassssment model, register the model in admin.py

--- prop_for_3d/admin.py
+++ prop_for_3d/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Prop
+from .models import Prop, Assessment
 
 # Register your models here.
 admin.site.register(Prop)
+admin.site.register(Assessment)
 
 
