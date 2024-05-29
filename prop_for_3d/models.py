5200a205927d4c6ac485907f40c43235beaeb71a
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-05-29 12:45:25 +0000

    Add Assessment model to models.py

--- prop_for_3d/models.py
+++ prop_for_3d/models.py
@@ -7,11 +7,17 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Prop(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="proposals")
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="proposals")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
 
-
+class Assessment(models.Model):
+    proposal = models.ForeignKey(Prop, on_delete=models.CASCADE, related_name="assessors_reply")
+    # slug = models.SlugField(max_length=200, unique=True)
+    assessor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assessment") # Must be someone else other than the 'User'?
+    comment = models.TextField()
+    approved_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
