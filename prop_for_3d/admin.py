ffa20019abe282ecd00362862527a798f191f16a
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-06-03 17:04:22 +0000

    Register Prop class with the decorator, test the Summernote is operational. Migrage db for summernote.

--- prop_for_3d/admin.py
+++ prop_for_3d/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from .models import Prop, Assessment
 from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(Prop)
 class PropAdmin(SummernoteModelAdmin): # Or AssessmentAdmin? The most likely, PropAdmin...
 
     list_display = ('title', 'slug', 'status') # N.b. Slug is not in a model. May throw an error
-    search_fields = ['title']
-    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')    
     prepopulated_fields = {'slug': ('title',)} # N.b. Slug is not in a model. May throw an error. Can I comment out/delete this line?
     summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Prop)
 admin.site.register(Assessment)
 
 
