0ba4449a73557e11c572dc27e5e6abedd53c7fcb
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-06-12 20:16:45 +0000

    Add layout to overview multiple proposals, updated the proposal model by adding fields 'keywords' and 'email'

--- prop_for_3d/models.py
+++ prop_for_3d/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Submitted"))
 
 # Create your models here.
 class Prop(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    keywords = models.CharField(max_length=300, unique=True)
+    email = models.EmailField(max_length=200)
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="proposals")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -24,6 +26,11 @@ class Assessment(models.Model):
     comment = models.TextField()
     approved_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    
+
+
+    
+
 
 
 
--- prop_for_3d/views.py
+++ prop_for_3d/views.py
@@ -5,15 +5,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-class PropList(generic.ListView):
-    model = Prop
+# class PropList(generic.ListView):
+#     model = Prop
 
 # def index(request):
 #     return HttpResponse('Hello, addictive!')
 
 class PropList(generic.ListView):
+    model = Prop
     queryset = Prop.objects.filter(status=1)
-    # template_name = "prop_list.html"
+    template_name = "proposal.html"
     template_name = "prop_for_3d/index.html"
 
 
