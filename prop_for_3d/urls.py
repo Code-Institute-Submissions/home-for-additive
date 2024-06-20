64eb8997e51e39fef90735e1d19c449c7ef6550a
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-06-20 16:54:37 +0000

    Replace all view functions with clacces (for consistency)

--- prop_for_3d/urls.py
+++ prop_for_3d/urls.py
@@ -1,17 +1,25 @@
 from . import views
 from django.urls import path
-from .views import UpdatePropView
+from .views import HomeView, TeamView, FacilityView, PropsView, SingleView, CreatePropView, UpdatePropView, DelePropView
 
 urlpatterns = [
-    path('', views.PropList.as_view(), name="home"),
-    path('index/', views.index, name="index"),
-    path('team/', views.team, name="team"),
-    path('facility/', views.facility, name="facility"),
-    path('proposal/', views.proposal, name="proposal"),
-    path('<slug:slug>/', views.prop_single, name="prop_single"),
-    path('proposal/new_prop/', views.submit_new_prop, name="new_prop"),
-    path('new_prop/confirmation/', views.submit_new_prop, name="submit_new_prop"), # name="?" check
-    path('update_prop/<int:pk>/', UpdatePropView.as_view(), name="update_prop")
-         #views.prop_edit, name='edit_prop'),    
+    path('', HomeView.as_view(), name="home"),
+    path('team/', TeamView.as_view(), name="team"),
+    path('facility/', FacilityView.as_view(), name="facility"),
+    path('proposals/', PropsView.as_view(), name="proposals"),
+    path('proposal/<int:pk>', SingleView.as_view(), name="prop_single"),
+    path('proposals/create_new', CreatePropView.as_view(), name="create_new"),
     ]
 
+
+
+
+#path('index/', views.index, name="index"),
+    #path('team/', views.team, name="team"),
+    #path('facility/', views.facility, name="facility"),
+    #path('proposals/', views.PropsView.as_view(), name="proposals"),
+    #path('<slug:slug>/', views.prop_single, name="prop_single"),
+    #path('proposal/new_prop/', views.submit_new_prop, name="new_prop"),
+    #path('new_prop/confirmation/', views.submit_new_prop, name="submit_new_prop"), # name="?" check
+    #path('update_prop/<int:pk>/', UpdatePropView.as_view(), name="update_prop")
+         #views.prop_edit, name='edit_prop'), 
--- prop_for_3d/views.py
+++ prop_for_3d/views.py
@@ -1,176 +1,74 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
-from django.contrib import messages
-from django.views.generic import UpdateView
-from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+#from django.contrib import messages
+#from django.http import HttpResponseRedirect
 from .models import Prop
 from .forms import NewProposal
 
-# Create your views here.
 
+# Classes for static pages:
 
-class PropList(generic.ListView):
+class HomeView(TemplateView):
     """
-    Model of proposal for proposed idea.
-    Returns all published posts in :model:`prop_for_3d.Prop`.
-    **Context**
-
-    ``queryset``
-        All published instances of :model:`prop_for_3d.Prop`
-
-    **Template:**
-
-    :template:`prop_for_3d/index.html`
+    Render and show starting ('Home') page
     """
-
-    model = Prop
-    queryset = Prop.objects.filter(status=1)
     template_name = "prop_for_3d/index.html"
 
-class UpdatePropView(UpdateView):
+
+class TeamView(TemplateView):
+    """
+    Render and show 'Meet our team' page.
+    """
+    template_name = "prop_for_3d/team.html"
+
+
+class FacilityView(TemplateView):
+    """
+    Render and show 'Facility' page.
+    """
+    template_name = "prop_for_3d/facility.html"
+
+
+# Classes wit dynamic content:
+
+
+class PropsView(ListView):
+    """
+    Shows page with all proposals in the list.
+    """
     model = Prop
-    template_name = "prop_for_3d/update_prop.html"
-    fields = ('title', 'keywords', 'content', )
+    template_name = "prop_for_3d/proposals.html"
 
 
-def index(request):
+class SingleView(DetailView):
     """
-    Display static 'home' page.
-
-    **Template:**
-
-    :template:`prop_for_3d/index.html`
-
+    Shows page with a single selected idea proposal.
     """
-    return render(request, 'prop_for_3d/index.html')
+    model = Prop
+    template_name = "prop_for_3d/prop_single.html"
 
 
-def team(request):
+class CreatePropView(CreateView):
     """
-    Display static 'team' page.
-
-    **Template:**
-
-    :template:`prop_for_3d/team.html`
-
+    Show a fillable form. The data can be stored in database as an entry.
     """
-    return render(request, 'prop_for_3d/team.html')
+    model = Prop
+    template_name = "prop_for_3d/new_prop.html"
+    fields = ('title', 'keywords', 'student', 'content')
 
 
-def facility(request):
+
+class UpdatePropView(UpdateView):
     """
-    Display static 'facility' page.
-
-    **Template:**
-
-    :template:`prop_for_3d/facility.html`
-
+    Update existing proposal.
     """
-    return render(request, 'prop_for_3d/facility.html')
+    model = Prop
+    #template_name
 
 
-def proposal(request):
+class DelePropView():
     """
-    Display an individual :model:`prop_for_3d.Prop`.
-
-    **Context**
-
-    ``proposal``
-        An instance of :model:`prop_for_3d.Prop`.
-
-    **Template:**
-
-    :template:`prop_for_3d/proposal.html`
-
+    Delete selected proposal. Check if the user is authorised
     """
-    proposals = Prop.objects.filter(status=1)
-    print(proposals)
-    return render(
-        request,
-        'prop_for_3d/proposal.html',
-        {'proposals': proposals},
-        )
 
 
-def prop_single(request, slug):
-    """
-    Display an individual :model:`prop_for_3d.Prop`.
-
-    **Context**
-
-    ``prop``
-        An instance of :model:`prop_for_3d.Prop`.
-
-    **Template:**
-
-    :template:`prop_for_3d/prop_single.html`
-
-    """
-    queryset = Prop.objects.filter(status=1)
-    prop = get_object_or_404(queryset, slug)
-    return render(
-        request,
-        'prop_for_3d/prop_single.html',
-        {"prop": prop, },
-        )
-
-
-def submit_new_prop(request):
-    """
-    Display, fill and submit the 'new proposal' form.
-
-    Form:
-    ``new_prop``
-    An instance of :form:`prop_for_3d/NewProposal`
-
-    **Template:**
-
-    :template:`prop_for_3d/prop_new_prop.html`
-    """
-    if request.method == "POST":
-        new_prop = NewProposal(data=request.POST)
-        #new_prop.slug = 'rrr' ## rrr - just testing
-        print(new_prop)
-        if new_prop.is_valid():
-            new_prop.save()
-            messages.add_message(
-                request, messages.SUCCESS, "Thanks for sharing your idea!")
-            new_prop = NewProposal()
-            return redirect('team')
-
-    content = {"new_prop": NewProposal(), }
-    print(content)
-    return render(request, 'prop_for_3d/new_prop.html', content)
-
-"""
-def prop_edit(request, slug):    
-    proposal = get_object_or_404(Prop, slug=slug)
-    if request.method == 'POST':
-        proposal.title = request.POST.get('title')
-        proposal.keywords = request.POST.get('keywords')
-        proposal.content = request.POST.get('content')
-        proposal.save()
-        return redirect('home')
-    context = {
-        'proposal':proposal
-    }
-    return render (request, 'edit_prop.html', context)
-        
-    
-
-    if request.method == "POST":
-        queryset = Prop.objects.filter(status=1)
-        proposal = get_object_or_404(queryset, slug=slug)
-        prop_form = NewProposal(data=request.POST, instance=proposal)
-        if prop_form.is_valid() and proposal.student == request.user: # .iser or .student?
-            proposal = prop_form.save(commit=False)
-            proposal.post = proposal
-            #proposal.approved = True # True or False?
-            proposal.save()
-            messages.add_message(request, messages.SUCCESS, 'Poposal Updated!')
-        else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
-
-    #return HttpResponseRedirect(reverse('prop_single', args=[slug]))
-    return redirect('proposal')
-    """
