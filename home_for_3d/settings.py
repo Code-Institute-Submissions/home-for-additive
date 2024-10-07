e9c3ef9dd408ecab374dc6c8938286b6c05824fa
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-10-07 08:14:23 +0000

    chore(deployment): prepare app for Heroku deployment

--- home_for_3d/settings.py
+++ home_for_3d/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = (
     'django-insecure-%*ly#4w0$!ef0plam8i6fkgedxbe88tz0fm^o6uiq7&f!_$(7#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
                  'localhost',
